e6b0067fcd3ce1252749aee69db24ed65b1dbc30
William <williamchenjun1708@gmail.com>
2026-05-17 13:54:34 +0200

    Added delete message shortcut method

--- components.js
+++ components.js
@@ -1936,6 +1936,27 @@ class Chat {
     async setChatMemberTag(config){
         return await Context.bot.setChatMemberTag({chat_id: this.id, ...config});
     }
+
+    /**
+     * Use this method to delete a message, including service messages, with the following limitations:
+    - A message can only be deleted if it was sent less than 48 hours ago.
+    - Service messages about a supergroup, channel, or forum topic creation can't be deleted.
+    - A dice message in a private chat can only be deleted if it was sent more than 24 hours ago.
+    - Bots can delete outgoing messages in private chats, groups, and supergroups.
+    - Bots can delete incoming messages in private chats.
+    - Bots granted `can_post_messages` permissions can delete outgoing messages in channels.
+    - If the bot is an administrator of a group, it can delete any message there.
+    - If the bot has can_delete_messages permission in a supergroup or a channel, it can delete any message there.
+
+    Shortcut of `bot.deleteMessage()`.
+
+    Returns `True` on success.
+     * @param {{message_id: number}} config 
+     * @returns {Promise<boolean>}
+     */
+    async deleteMessage(config) {
+        return await Context.bot.deleteMessage({chat_id: this.id, ...config})
+    }
 }
 
 /**
