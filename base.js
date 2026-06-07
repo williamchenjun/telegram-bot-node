fe9783b0f2601c5cb91cd0f5a7e950cc82b7961c
William Chen <79821802+williamchenjun@users.noreply.github.com>
2026-06-07 11:47:13 +0200

    Added new methods

--- base.js
+++ base.js
@@ -1573,7 +1573,7 @@ class Bot {
      * @returns {Promise<Sticker[]>}
      */
     async getForumTopicIconStickers(){
-        let params = App.HTTP({ method: "getForumTopicIconStickers", params: config });
+        let params = App.HTTP({ method: "getForumTopicIconStickers" });
         const response = await fetchWithTimeout(this.endpoint, params, 20000);
 
         if (!response.ok) {
